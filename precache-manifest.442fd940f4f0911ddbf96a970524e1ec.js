cb805415150db46b9c726243003a5b2faecc17da
伍程凯 <wuchengkai0@gmail.com>
2018-06-04 17:25:10 +0800

    Updates

--- precache-manifest.442fd940f4f0911ddbf96a970524e1ec.js
+++ precache-manifest.442fd940f4f0911ddbf96a970524e1ec.js
@@ -1,46 +1,46 @@
 self.__precacheManifest = [
   {
     "revision": "197b2fcc9aaa3c0e0effc377440d7d8d",
-    "url": "/assets/images/upyun.197b2fcc.jpg"
+    "url": "/vov/assets/images/upyun.197b2fcc.jpg"
   },
   {
     "revision": "4dee59e6742ad7d2ec89",
-    "url": "/vendor.4dee59e6.js"
+    "url": "/vov/vendor.4dee59e6.js"
   },
   {
     "revision": "4dee59e6742ad7d2ec89",
-    "url": "/vendor.3b918fd2.css"
+    "url": "/vov/vendor.3b918fd2.css"
   },
   {
-    "revision": "74d00f5ff450af49afc71f29b16079cd",
-    "url": "/index.html"
-  },
-  {
-    "revision": "18c5949af827dcf8af14",
-    "url": "/client.f61bcfc6.css"
+    "revision": "d7d0dc8d57b341e9058c47e54ba23b79",
+    "url": "/vov/index.html"
   },
   {
     "revision": "18c5949af827dcf8af14",
-    "url": "/client.18c5949a.js"
+    "url": "/vov/client.f61bcfc6.css"
+  },
+  {
+    "revision": "18c5949af827dcf8af14",
+    "url": "/vov/client.18c5949a.js"
   },
   {
     "revision": "0bd90e97b5e6db764d2641afa78739ac",
-    "url": "/assets/images/tencent.0bd90e97.jpg"
+    "url": "/vov/assets/images/tencent.0bd90e97.jpg"
   },
   {
     "revision": "d02be7023d9d56a604bc2b8f738b3e3c",
-    "url": "/assets/images/imgur.d02be702.jpg"
+    "url": "/vov/assets/images/imgur.d02be702.jpg"
   },
   {
     "revision": "affdf736f26b72ae8bcba59c55d21b8b",
-    "url": "/assets/images/flickr.affdf736.jpg"
+    "url": "/vov/assets/images/flickr.affdf736.jpg"
   },
   {
     "revision": "391cb92c37f50780858a2a07643f6a03",
-    "url": "/assets/images/default.391cb92c.svg"
+    "url": "/vov/assets/images/default.391cb92c.svg"
   },
   {
     "revision": "9f669c219265599151cf5e97e9b505c3",
-    "url": "/assets/images/aws.9f669c21.png"
+    "url": "/vov/assets/images/aws.9f669c21.png"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.515fc0fe7ca61499f2c194689ae97f5b.js"
+  "/vov/precache-manifest.442fd940f4f0911ddbf96a970524e1ec.js"
 );
 
 /**
