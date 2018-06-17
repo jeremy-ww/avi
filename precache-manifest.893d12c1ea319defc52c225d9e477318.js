2b4ae03dfe1739e3731ca6b6d91c7eef2ce4178d
伍程凯 <wuchengkai0@gmail.com>
2018-06-17 21:48:32 +0800

    Updates

---
+++ precache-manifest.893d12c1ea319defc52c225d9e477318.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "affdf736f26b72ae8bcba59c55d21b8b",
+    "url": "/assets/images/flickr.affdf736.jpg"
+  },
+  {
+    "revision": "e41d94241ea8d6c20cf0",
+    "url": "/vendor.e41d9424.js"
+  },
+  {
+    "revision": "6efd28c15321d49af8f23f18d57b069a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d00560d2986fc217893a0652bc2f04d",
+    "url": "/homescreen-96.png"
+  },
+  {
+    "revision": "9564b538a0465d637cbccb22d0be55b1",
+    "url": "/locales/en/translations.json"
+  },
+  {
+    "revision": "ddb298f8ef632ef07802a3dd850deaf3",
+    "url": "/locales/zh/translations.json"
+  },
+  {
+    "revision": "e41d94241ea8d6c20cf0",
+    "url": "/vendor.3b918fd2.css"
+  },
+  {
+    "revision": "25ef2810b4ef46e56c79cd15066d8ae5",
+    "url": "/homescreen-168.png"
+  },
+  {
+    "revision": "aacbd614a98ef8be5123",
+    "url": "/client.aacbd614.js"
+  },
+  {
+    "revision": "ef996c580ad48ba587f53b522a4053fc",
+    "url": "/homescreen-72.png"
+  },
+  {
+    "revision": "2833188940609fc1bfa25cbc06384b82",
+    "url": "/homescreen-192.png"
+  },
+  {
+    "revision": "928763e27fa6ee915004570ed48b372f",
+    "url": "/homescreen-144.png"
+  },
+  {
+    "revision": "805830c4d48151b31a29b57f73d242b9",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "aacbd614a98ef8be5123",
+    "url": "/client.8dfad88d.css"
+  },
+  {
+    "revision": "197b2fcc9aaa3c0e0effc377440d7d8d",
+    "url": "/assets/images/upyun.197b2fcc.jpg"
+  },
+  {
+    "revision": "0bd90e97b5e6db764d2641afa78739ac",
+    "url": "/assets/images/tencent.0bd90e97.jpg"
+  },
+  {
+    "revision": "d02be7023d9d56a604bc2b8f738b3e3c",
+    "url": "/assets/images/imgur.d02be702.jpg"
+  },
+  {
+    "revision": "391cb92c37f50780858a2a07643f6a03",
+    "url": "/assets/images/default.391cb92c.svg"
+  },
+  {
+    "revision": "9f669c219265599151cf5e97e9b505c3",
+    "url": "/assets/images/aws.9f669c21.png"
+  }
+];
