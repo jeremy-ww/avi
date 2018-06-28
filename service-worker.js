6289bf830a685ea9fa5a3c21f25ecd6bd221c21a
伍程凯 <wuchengkai0@gmail.com>
2018-06-28 12:00:42 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.623d1cc1be96586d7b0d7c69188892d4.js"
+  "/precache-manifest.cc2ef687e1d28b73320f1ff44686b01b.js"
 );
 
 /**
